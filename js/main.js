1c21e4fe6acb7cac01358a0fd7eda2607d84f331
Elleven <eleonoramanushkova@yandex.ru>
2023-03-15 23:23:34 +0700

    3 column

--- js/main.js
+++ js/main.js
@@ -32,11 +32,15 @@ Vue.component('column', {
         localSaveSecondColumn(){
             localStorage.setItem('column_2', JSON.stringify(this.column_2));
         },
+        localThirdSecondColumn(){
+            localStorage.setItem('column_3', JSON.stringify(this.column_3));
+        },
     },
 
     mounted() {
         this.column_1= JSON.parse(localStorage.getItem("column_1")) || [];
         this.column_2 = JSON.parse(localStorage.getItem("column_2")) || [];
+        this.column_3 = JSON.parse(localStorage.getItem("column_3")) || [];
 
         eventBus.$on('addColumn_1', card => {
             this.column_1.push(card)
@@ -45,9 +49,11 @@ Vue.component('column', {
         })
         eventBus.$on('addColumn_2', card => {
             this.column_2.push(card)
-            this.localSaveSecondColumn();
-
-
+            this.localSaveSecondColumn()
+        })
+        eventBus.$on('addColumn_3', card => {
+            this.column_3.push(card)
+            this.localThirdSecondColumn()
         })
     },
 
@@ -235,23 +241,65 @@ Vue.component('column_3', {
     },
     template: `
         <section id="main" class="main-alt">
-            <div class="column column_three">
-                <p>Завершенные задачи</p>
+            <div class="column column__three">
+            <p>Тестирование</p>
                 <div class="card" v-for="card in column_3">
-                <h3>{{ card.name }}</h3>
-                    <ul class="tasks" v-for="task in card.points"
-                        v-if="task.name != null"
-                        @click="TaskCompleted(card, task)"
-                        :class="{completed: task.completed}">
-                        <li>
-                        {{ task.name }}
-                        </li>
-                    </ul><br>
-                    <p class="date">{{ card.date }}</p>
+                    <br>
+                   <div class="tasks">Название: {{ card.name }}</div>
+                    <div class="tasks">Описание: {{ card.description }}</div>
+                    <div class="tasks">Дата создания: {{ card.date }}</div>
+                    <div class="tasks">Крайний срок: {{ card.deadline }}</div>
+                    <div class="tasks" v-if="card.reason.length">Причина переноса: <p v-for="reason in card.reason">{{ reason }}</p></div>
+                    <div class="tasks" v-if="card.editDate != null">Последнее изменение: {{ card.editDate }}</div>
+                    <div class="tasks" v-if="card.edit">
+                        <form @submit.prevent="updateTask(card)">
+                            <p style="font-size: ">Новое название: 
+                                <input type="text" v-model="card.name" placeholder="Название">
+                            </p>
+                            <p>Новое описание: 
+                                <textarea v-model="card.description"></textarea>
+                            </p>
+                            <p>
+                                <input type="submit" class="btn" value="Изменить карточку">
+                            </p>
+                        </form>
+                    </div>
+                    <div class="tasks" v-if="card.transfer"><br>
+                        <form @submit.prevent="lastColumn(card)">
+                            <p>Причина переноса:
+                                <input type="text" id="reasonInput">
+                            </p><br>
+                            <p>
+                                <input class="per" type="submit" value="Перенос">
+                            </p>
+                        </form>
+                    </div>
+                    <a @click="card.edit = true" class="red">Редактировать</a> <br>
+                    <a @click="card.transfer = true" class="back">Предыдущая колонка</a><br>
+                    <a @click="nextColumn(card)">Следующая колонка</a>
                 </div>
             </div>
         </section>
     `,
+    methods: {
+        lastColumn(card) {
+            let reasonValue = document.getElementById('reasonInput').value;
+            card.reason.push(reasonValue)
+            card.transfer = false
+            this.column_3.splice(this.column_3.indexOf(card), 1)
+            eventBus.$emit('addColumn_2', card)
+        },
+        updateTask(card){
+            card.editDate = new Date().toLocaleString()
+            card.edit = false
+            this.column_3.push(card)
+            this.column_3.splice(this.column_3.indexOf(card), 1)
+        },
+        nextColumn(card) {
+            this.column_3.splice(this.column_3.indexOf(card), 1)
+            eventBus.$emit('addColumn_4', card)
+        },
+    }
 })
 
 Vue.component('column_4', {
